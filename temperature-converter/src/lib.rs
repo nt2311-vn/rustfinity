d388d259583e1adc9869ab49b5cd529ce1550109
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-24 16:00:18 +0700

    solved another way

--- temperature-converter/src/lib.rs
+++ temperature-converter/src/lib.rs
@@ -1,29 +1,37 @@
 
-pub fn convert_temperature(value: f64, from_unit: &str, to_unit: &str) -> Result<f64, String> {
-    if !check_inout(from_unit, to_unit) {
-        Err(String::from("Invalid unit"))
-    } else {
-        Ok(perform_convert(value, from_unit, to_unit))
-    }
+enum TUnit {
+    C,
+    F,
+    K,
+    Invalid
 }
 
-pub fn check_inout(from_unit: &str, to_unit: &str) -> bool {
-    match (from_unit, to_unit) {
-        ("C" | "K" | "F", "C" | "K" | "F") =>  true,
-        _ => false
-    }
-}
-
-
-pub  fn perform_convert(value: f64,from_unit: &str, to_unit: &str) -> f64 {
-        match (from_unit, to_unit) {
-            ("F", "C") => (value - 32.0) * (5.0/9.0) ,
-            ("F", "K") => (value - 32.0) * (5.0/9.0) + 273.15,
-            ("C", "F") => value * (9.0 / 5.0) + 32.0,
-            ("C", "K") => value + 273.15,
-            ("K", "C") => value - 273.15,
-            ("K", "F") => (value -273.15) * (9.0 / 5.0) + 32.0,
-            _ => value
-
+impl From<&str> for TUnit {
+    fn from(value: &str) -> TUnit {
+        match value  {
+            "C" => TUnit::C,
+            "F" => TUnit::F,
+            "K" => TUnit::K,
+            _ =>  TUnit::Invalid
         }
+    }
 }
+
+pub fn convert_temperature(value: f64, from_unit: &str, to_unit: &str) -> Result<f64, String> {
+    let from = TUnit::from(from_unit);
+    let to = TUnit::from(to_unit);
+
+    match (from, to) {
+        (TUnit::Invalid, _) | (_, TUnit::Invalid) => Err(String::from("Invalid unit")),
+        (TUnit::F, TUnit::C) => Ok((value - 32.0) * (5.0/9.0)),
+        (TUnit::F, TUnit::K) => Ok((value - 32.0) * (5.0/9.0) + 273.15),
+        (TUnit::C, TUnit::F) => Ok(value * (9.0 / 5.0) + 32.0),
+        (TUnit::C, TUnit::K) => Ok(value + 273.15),
+        (TUnit::K, TUnit::C) => Ok(value - 273.15),
+        (TUnit::K, TUnit::F) => Ok((value -273.15) * (9.0 / 5.0) + 32.0),
+        _ => Ok(value)
+    }
+    
+}
+
+
